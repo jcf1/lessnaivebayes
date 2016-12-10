a0ec33660388c5e36cab362f91a0c52a56c223cf
Muh Muhten <muh.muhten@gmail.com>
2016-12-09 20:54:28 -0500

    whitespace changes

--- java/nlp/Datapoint.java
+++ java/nlp/Datapoint.java
@@ -17,79 +17,79 @@ import java.io.*;
  */
 public class Datapoint {
 
-    /** The text represented as a map from the word to the number of occurrences. */
-    private HashMap<String,Integer> word = new HashMap<String,Integer>();
+	/** The text represented as a map from the word to the number of occurrences. */
+	private HashMap<String,Integer> word = new HashMap<String,Integer>();
 
-    private HashMap<String,HashMap<String,Integer>> bigramCount = new HashMap<String,HashMap<String,Integer>>();
+	private HashMap<String,HashMap<String,Integer>> bigramCount = new HashMap<String,HashMap<String,Integer>>();
 
-    /** The total number of words in the text. */
-    public int noOfWords;
+	/** The total number of words in the text. */
+	public int noOfWords;
 
-    /** The category. */
-    public String cat;
+	/** The category. */
+	public String cat;
 
-    public Datapoint( String text, String cat ) {
-	this.cat = cat;
-	try {
-	    StringReader reader = new StringReader( text + "\n" );
-	    Tokenizer tok = new Tokenizer( reader );
-      String lastWord = "";
-	    while ( tok.hasMoreTokens() ) {
-    		noOfWords++;
-    		String w = tok.nextToken();
-    		Integer count = word.get( w );
-    		if ( count == null ) {
-    		    count = 1;
-    		}
-    		else {
-    		    count++;
-    		}
-    		word.put( w, count );
+	public Datapoint( String text, String cat ) {
+		this.cat = cat;
+		try {
+			StringReader reader = new StringReader( text + "\n" );
+			Tokenizer tok = new Tokenizer( reader );
+			String lastWord = "";
+			while ( tok.hasMoreTokens() ) {
+				noOfWords++;
+				String w = tok.nextToken();
+				Integer count = word.get( w );
+				if ( count == null ) {
+					count = 1;
+				}
+				else {
+					count++;
+				}
+				word.put( w, count );
 
-        HashMap<String,Integer> counts = bigramCount.get(lastWord);
-        if(counts == null) {
-          counts = new HashMap<String,Integer>();
-        }
-        count = counts.get(w);
-        if(count == null) {
-          count = 1;
-        } else {
-          count++;
-        }
-        counts.put(w, count);
-        bigramCount.put(lastWord, counts);
-        lastWord = w;
-	    }
+				HashMap<String,Integer> counts = bigramCount.get(lastWord);
+				if(counts == null) {
+					counts = new HashMap<String,Integer>();
+				}
+				count = counts.get(w);
+				if(count == null) {
+					count = 1;
+				} else {
+					count++;
+				}
+				counts.put(w, count);
+				bigramCount.put(lastWord, counts);
+				lastWord = w;
+			}
+		}
+		catch ( IOException e ) {
+			e.printStackTrace();
+		}
 	}
-	catch ( IOException e ) {
-	    e.printStackTrace();
+
+
+	public Iterator<String> iterator() {
+		return word.keySet().iterator();
 	}
-    }
 
+	public Integer count( String w ) {
+		return word.get( w );
+	}
 
-    public Iterator<String> iterator() {
-	return word.keySet().iterator();
-    }
+	public HashMap<String,Integer> getBigrams(String word) {
+		return bigramCount.get(word);
+	}
 
-    public Integer count( String w ) {
-	return word.get( w );
-    }
+	public Integer countBigram(String w1, String w2) {
+		HashMap<String,Integer> counts = bigramCount.get(w1);
+		if(counts == null) {
+			return null;
+		}
+		//System.out.println(w1 + " " + w2 + " " + bigramCount);
+		return counts.get(w2);
+	}
 
-    public HashMap<String,Integer> getBigrams(String word) {
-      return bigramCount.get(word);
-    }
-
-    public Integer countBigram(String w1, String w2) {
-      HashMap<String,Integer> counts = bigramCount.get(w1);
-      if(counts == null) {
-        return null;
-      }
-      //System.out.println(w1 + " " + w2 + " " + bigramCount);
-      return counts.get(w2);
-    }
-
-    public String toString() {
-	return word.toString();
-    }
+	public String toString() {
+		return word.toString();
+	}
 
 }
--- java/nlp/Dataset.java
+++ java/nlp/Dataset.java
@@ -16,78 +16,78 @@ import java.util.*;
  */
 public class Dataset {
 
-    /** The number of categories (=classes). */
-    int noOfCategories;
+	/** The number of categories (=classes). */
+	int noOfCategories;
 
-    /** The mapping from category identifiers to category names. */
-    String[] catName;
+	/** The mapping from category identifiers to category names. */
+	String[] catName;
 
-    /** The mapping from category names to category identifiers. */
-    HashMap<String,Integer> catIndex = new HashMap<String,Integer>();
+	/** The mapping from category names to category identifiers. */
+	HashMap<String,Integer> catIndex = new HashMap<String,Integer>();
 
-    /** The number of data points (=texts). */
-    public int totNoOfDatapoints;
+	/** The number of data points (=texts). */
+	public int totNoOfDatapoints;
 
-    /** The number of data points per category. */
-    public int[] noOfDatapoints;
+	/** The number of data points per category. */
+	public int[] noOfDatapoints;
 
-    /** The number of words per category. */
-    public int[] noOfWords;
+	/** The number of words per category. */
+	public int[] noOfWords;
 
-    /** The datapoints. */
-    public ArrayList<Datapoint> point = new ArrayList<Datapoint>();
+	/** The datapoints. */
+	public ArrayList<Datapoint> point = new ArrayList<Datapoint>();
 
-    public Dataset( String filename ) {
-	try {
-	    BufferedReader in = new BufferedReader( new FileReader( filename ));
-	    String line = null; 
-	    Scanner scan = new Scanner( in );
-	    // Read the categories
-	    noOfCategories = scan.nextInt();
-	    catName = new String[noOfCategories];
-	    noOfDatapoints = new int[noOfCategories];
-	    noOfWords = new int[noOfCategories];
-	    for ( int i=0; i<noOfCategories; i++ ) {
-		String name = scan.next(); 
-		catName[i] = name;
-		catIndex.put( name, i );
-	    }
-	    // Read the datapoints
-	    totNoOfDatapoints = scan.nextInt();
-	    for ( int i=0; i<totNoOfDatapoints; i++ ) {
-		String category = scan.next();
-		Integer cIndex = catIndex.get( category );
-		String text = scan.nextLine();
-		if ( cIndex == null ) {
-		    // This category does not exist! 
-		    // (probably a spelling error in the training file)
-		    System.err.println( "Warning: Unknown category " + category + " for datapoint number " + i );
-		    continue;
+	public Dataset( String filename ) {
+		try {
+			BufferedReader in = new BufferedReader( new FileReader( filename ));
+			String line = null; 
+			Scanner scan = new Scanner( in );
+			// Read the categories
+			noOfCategories = scan.nextInt();
+			catName = new String[noOfCategories];
+			noOfDatapoints = new int[noOfCategories];
+			noOfWords = new int[noOfCategories];
+			for ( int i=0; i<noOfCategories; i++ ) {
+				String name = scan.next(); 
+				catName[i] = name;
+				catIndex.put( name, i );
+			}
+			// Read the datapoints
+			totNoOfDatapoints = scan.nextInt();
+			for ( int i=0; i<totNoOfDatapoints; i++ ) {
+				String category = scan.next();
+				Integer cIndex = catIndex.get( category );
+				String text = scan.nextLine();
+				if ( cIndex == null ) {
+					// This category does not exist! 
+					// (probably a spelling error in the training file)
+					System.err.println( "Warning: Unknown category " + category + " for datapoint number " + i );
+					continue;
+				}
+				if ( text!=null && category!=null ) {
+					Datapoint dp = new Datapoint( text, category );
+					point.add( dp );
+					noOfWords[cIndex] += dp.noOfWords;
+					noOfDatapoints[cIndex]++;
+				}
+			}
 		}
-		if ( text!=null && category!=null ) {
-		    Datapoint dp = new Datapoint( text, category );
-		    point.add( dp );
-		    noOfWords[cIndex] += dp.noOfWords;
-		    noOfDatapoints[cIndex]++;
+		catch ( IOException e  ) {
+			e.printStackTrace();
 		}
-	    }
 	}
-	catch ( IOException e  ) {
-	    e.printStackTrace();
-	}
-    }
 
-    public Iterator<Datapoint> iterator() {
-	return point.iterator();
-    }
-
-    public String toString() {
-	StringBuffer buf = new StringBuffer();
-	Iterator<Datapoint> iter = iterator();
-	while ( iter.hasNext() ) {
-	    buf.append( iter.next().toString() );
-	    buf.append( "\n" );
+	public Iterator<Datapoint> iterator() {
+		return point.iterator();
 	}
-	return buf.toString();
-    }
+
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		Iterator<Datapoint> iter = iterator();
+		while ( iter.hasNext() ) {
+			buf.append( iter.next().toString() );
+			buf.append( "\n" );
+		}
+		return buf.toString();
+	}
 }
