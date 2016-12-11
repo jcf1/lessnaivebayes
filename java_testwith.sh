74f76085dfd505d6f3a5cdfde9344fae23615a83
Muh Muhten <muh.muhten@gmail.com>
2016-12-11 12:29:57 -0500

    move outfiles to a tmp dir and make it reentrant

--- java_testwith.sh
+++ java_testwith.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec 2> tmp/out.x
+exec 2> tmp/out$$.x
 set -x
 javac -Xlint -cp java -d java/classes java/nlp/*.java
 
@@ -8,9 +8,9 @@ grams='1 2'
 pids=''
 for gram in $grams; do
 	time {
-		java -cp java/classes nlp.NaiveBayesTextClassifier "-$gram" -d "$1" -t "$2" > "tmp/out.$gram"
-		diff "$2" "tmp/out.$gram" > "tmp/out.$gram.wrong"
-		num=`grep '^>' "tmp/out.$gram.wrong" | wc -l | awk '{print $1}'`
+		java -cp java/classes nlp.NaiveBayesTextClassifier "-$gram" -d "$1" -t "$2" > "tmp/out$$.$gram"
+		diff "$2" "tmp/out$$.$gram" > "tmp/out$$.$gram.wrong"
+		num=`grep '^>' "tmp/out$$.$gram.wrong" | wc -l | awk '{print $1}'`
 		sum=`wc -l "$2" | awk '{print $1}'`
 		pct=$((100*num/sum))
 		dec=$((10000*num/sum-100*pct))
